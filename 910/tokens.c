452ccd663dbaf0aaeda02b032e2f46229a0faec1
Braden Helmer <brhelmer@cisco.com>
2022-07-05 01:09:18 -0400

    Directory change

---
+++ 910/tokens.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+enum ARTITHMETIC_OPERATORS {
+    ADD = "+",
+    SUBTRACT = "-",
+    MULTIPLY = "*",
+    DIVIDE = "/",
+    EXPONENT = "^",
+    INTEGER_DIVISION = "//",
+    MODULUS = "\%",
+}
+
